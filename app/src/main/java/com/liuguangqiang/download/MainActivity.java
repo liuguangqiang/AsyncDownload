2f9bd4605a4da8e1a551c816da001a133d792cd2
Eric <guangqiang.dev@gmail.com>
2014-08-13 18:01:05 +0800

    Update the demo.

--- app/src/main/java/com/liuguangqiang/download/MainActivity.java
+++ app/src/main/java/com/liuguangqiang/download/MainActivity.java
@@ -5,6 +5,7 @@ import java.io.File;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.liuguangqiang.download.core.AsyncDownload;
 import com.liuguangqiang.download.core.DownloadConfiguration;
@@ -20,47 +21,54 @@ public class MainActivity extends Activity {
 
     private String testUrl = "http://dl.fishsaying.com/fishsaying_1.8.1.apk";
 
+    private TextView tvProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvProgress=(TextView)this.findViewById(R.id.tv_progress);
         init();
         testDownload();
     }
 
     private void init(){
         AsyncDownload.getInstance().init(
-                new DownloadConfiguration.Builder().newSingleThreadExecutor().build());
+                new DownloadConfiguration.Builder().setSingleThreadExecutor().build());
 
         filePath = FileUtils.getSdcardPath() + "/AsyncDownload";
         File file = new File(filePath);
         if (!file.exists()) file.mkdirs();
     }
-
+    DownloadParams params;
     private void testDownload() {
         String pathFormat = filePath + "/t%s.apk";
-        DownloadParams params;
+
         String savePath;
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 1; i++) {
             savePath = String.format(pathFormat, i + 1);
             params = new DownloadParams(testUrl, savePath);
             params.setTag("Tag"+(i+1));
             AsyncDownload.getInstance().download(params, new DownloadListener() {
 
                 @Override
-                public void onStart(DownloadParams params) {
-
-                    Log.i(TAG,params.getTag() + " download start");
+                public void onStart() {
+                    Log.i(TAG,"download start");
                 }
 
                 @Override
-                public void onSuccess(DownloadParams params) {
-                    Log.i(TAG,params.getTag() + " download success");
+                public void onSuccess() {
+                    Log.i(TAG,"download success");
                 }
 
                 @Override
-                public void onProgressUpdate(int progress, DownloadParams params) {
-                    Log.i(TAG,params.getTag() + " progress--->"+progress);
+                public void onProgressUpdate(int progress) {
+                    tvProgress.setText("progress--->"+progress);
+                }
+
+                @Override
+                public void onFailure(String msg) {
+                    Log.i(TAG,"下载失败 : " +msg);
                 }
             });
         }
