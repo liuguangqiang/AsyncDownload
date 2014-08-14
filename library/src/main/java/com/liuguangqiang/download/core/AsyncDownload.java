cac315d76ffe89ee00a3a42103f0398a5b752446
Eric <guangqiang.dev@gmail.com>
2014-08-14 11:16:22 +0800

    Added License to the top.

--- library/src/main/java/com/liuguangqiang/download/core/AsyncDownload.java
+++ library/src/main/java/com/liuguangqiang/download/core/AsyncDownload.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Eric Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.liuguangqiang.download.core;
 
 import java.util.concurrent.ExecutorService;
@@ -10,7 +26,7 @@ import android.net.http.AndroidHttpClient;
 
 /**
  *
- * AsyncDownload
+ * Singleton for downloading.
  *
  * Created by Eric on 2014-8-12
  */
@@ -22,7 +38,7 @@ public class AsyncDownload {
     private HttpParams mHttpParams;
 
 
-    protected AsyncDownload() {};
+    private AsyncDownload() {};
 
 
     private volatile static AsyncDownload instance;
@@ -70,13 +86,19 @@ public class AsyncDownload {
         download(params, null);
     }
 
+    /**
+     * start download.
+     *
+     * @param params
+     * @param listener
+     */
     public void download(DownloadParams params, DownloadListener listener) {
         if (!isInited()) {
             throw new IllegalStateException(
                     "AsyncDownload must be initialized with configuration before using");
         }
         if (params == null) {
-            throw new NullPointerException("call download() with null parameter.");
+            throw new NullPointerException("DownloadParams must not be null.");
         } else {
             DownloadTask task = new DownloadTask(mHttpClient, params, listener);
             executorService.submit(task);
--- library/src/main/java/com/liuguangqiang/download/core/DownloadConfiguration.java
+++ library/src/main/java/com/liuguangqiang/download/core/DownloadConfiguration.java
@@ -1,6 +1,20 @@
-package com.liuguangqiang.download.core;
+/*
+ * Copyright 2014 Eric Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import android.text.TextUtils;
+package com.liuguangqiang.download.core;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -24,6 +38,10 @@ public final class DownloadConfiguration {
         this.socketTimeout = builder.socketTimeout;
     }
 
+    /**
+     * Return default DownloadConfiguration.
+     * @return
+     */
     public static DownloadConfiguration createDefault() {
         return new Builder().build();
     }
